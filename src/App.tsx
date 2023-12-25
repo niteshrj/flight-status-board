c838c34cfa6052e69a546c6e3ba822a25c3f96ea
niteshr <niteshr@thoughtworks.com>
2023-12-26 04:28:31 +0530

    Add page not found route and page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {FlightStatusTable} from "./components/FlightStatusTable";
 import {Route, Routes} from "react-router-dom";
 import {FlightDetails} from "./components/FlightDetails";
+import {NotFoundPage} from "./error/NotFoundPage";
 
 function App() {
   return (
         <Routes>
           <Route path="/flight-status" element={<FlightStatusTable/>}/>
           <Route path="/flight-status/:id" element={<FlightDetails/>}/>
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
   )
 }
